1afd753507633e7e086edfffba82d85a69e9f843
Shahbaz Ansari <96674638+shahbaz4783@users.noreply.github.com>
2022-10-30 20:30:51 +0530

    testing touch controls

--- js/script.js
+++ js/script.js
@@ -155,5 +155,40 @@ window.addEventListener('keydown', e=>{
      
 });
 
+//touch controls
 
+window.addEventListener('touchstart', e=>{
+    snakeVel= {x:0, y:1}
+    moveSound.play();
+    // themeSound.play();
+    switch (e.changedTouches) {
+        case "SwipedUp":
+            console.log("swiped up!")
+            snakeVel.x = 0;
+            snakeVel.y = -1;
+            break;
+    
+        case "SwipedDown":
+            console.log("swiped down!")
+            snakeVel.x = 0;
+            snakeVel.y = 1;
+            break;
+    
+        case "SwipedLeft":
+            console.log("swiped left!")
+            snakeVel.x = -1;
+            snakeVel.y = 0;
+            break;
+    
+        case "SwipedRight":
+            console.log("swiped right!")
+            snakeVel.x = 1;
+            snakeVel.y = 0;
+            break;
+    
+        default:
+            break;
+    }
+     
+});
 
